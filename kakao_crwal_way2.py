73458c4fb8ed102ac9ac2df36f013ad69ad51905
Hanbooyo <qnfudk@gmail.com>
2023-05-01 17:44:27 +0900

    스프레드시트 저장메소드 수정(값이있으면 빈 행으로 작성), 호출 조건 추가(날짜로 시작하거나 공백일때는 입력이 끝난걸로 간주)

--- kakao_crwal_way2.py
+++ kakao_crwal_way2.py
@@ -19,97 +19,84 @@ sheet_name='테스트중'
 # 구글 시트 API 사용 객체 생성
 service = build('sheets', 'v4', credentials=creds)
 
-# 메시지에서 데이터 추출 및 시트에 추가하는 함수
-def add_message_data_to_sheet_continuously(data):
+# 메시지에서 데이터 추출하는 함수
+def extract_data_from_message(data):
+    name = data.get('name', '')
+    age = data.get('age', '')
+    address = data.get('address', '')
+    running_time = data.get('running_time', '')
+    running_pace = data.get('running_pace', '')
+    entry_path = data.get('entry_path', '')
+    self_ex = data.get('self_ex', '')
+
+    return [name, age, address, running_time, running_pace, entry_path, self_ex]
+
+# 시트에 데이터 추가하는 함수
+def append_data_to_sheet(values):
     service = build('sheets', 'v4', credentials=creds)
-    try:
-        # 메시지에서 데이터 추출
-        name = data['name'] 
-        birth_year = data['age']
-        address = data['address']
-        running_time = data['running_time']
-        running_pace = data['running_exp']
-        entry_path = data['entry_path']
+    sheet_name = '자기소개모음'  # 시트 이름
+    range_name = f'{sheet_name}!A:G'
+    value_input_option = 'USER_ENTERED'
 
-        # 시트에 데이터 추가
-        sheet_values = [[name, birth_year, address, running_time, running_pace, entry_path]]
-        request_body = {
-            'values': sheet_values
-        }
-        sheet_name = "테스트중" # 시트 이름
-        value_input_option = 'USER_ENTERED'
-        insert_data_option = 'INSERT_ROWS'
-        service.spreadsheets().values().append(
-            spreadsheetId=SPREADSHEET_ID,
-            range=f"{sheet_name}!A:A",
-            valueInputOption=value_input_option,
-            insertDataOption=insert_data_option,
-            body=request_body
-        ).execute()
+    # 시트 데이터 가져오기
+    result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=range_name).execute()
+    values = result.get('values', [])
 
-        # 시트 데이터 가져오기
-        result = service.spreadsheets().values().get(
-            spreadsheetId=SPREADSHEET_ID, range=sheet_name).execute()
-        values = result.get('values', [])
+    # 마지막 행 찾기
+    last_row = len(values) + 1
 
-        # 마지막 행 찾기
-        last_row = len(values) + 1
+    # 새 데이터 추가할 범위 설정
+    new_range = f'{sheet_name}!A{last_row}:G{last_row}'
 
-        # 새 데이터 추가할 범위 설정
-        new_range = f'{sheet_name}!A{last_row}:G{last_row}'
+    # 새 데이터 설정
+    new_values = [extract_data_from_message(data)]
 
-        # 새 데이터 설정
-        new_values = [[name, birth_year, address, running_time, running_pace, entry_path]]
+    # 시트에 새 데이터 추가
+    request = service.spreadsheets().values().update(
+        spreadsheetId=SPREADSHEET_ID, range=new_range,
+        valueInputOption=value_input_option, body={'values': new_values}).execute()
 
-        # 시트에 새 데이터 추가
-        request = service.spreadsheets().values().update(
-            spreadsheetId=SPREADSHEET_ID, range=new_range,
-            valueInputOption='RAW', body={'values': new_values}).execute()
+    print(f'{request.get("updatedCells")} cells updated.')
 
-        print(f'{request.get("updatedCells")} cells updated.')
+# 메시지에서 추출한 데이터를 시트에 추가
+def add_message_data_to_sheet(data):
+    values = extract_data_from_message(data)
+    append_data_to_sheet(values)
 
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-with open('./Talk_2023.5.1 12_15-1.txt', 'r', encoding="utf-8") as f:
+with open('./Talk_2023.5.1 12_15-4.txt', 'r', encoding="utf-8") as f:
     data = {}
     for line in f:
         line = line.strip()
-        print(line)
         if "1)이름" in line: #startswith가 아니었음
             try:
                 data['name'] = line.split(":")[3].strip()
                 line = f.readline().strip()
                 if data['name'] != "":
                     print("name",data['name'])
-                    time.sleep(3)
             except IndexError:
                 continue
             if data['name'] and data['name'] != '방장봇':
                 if line.startswith('2)'):
                     data['age'] = line.split(':')[1].strip()
-                    print(data['age'])
                     line = f.readline().strip()
                     if line.startswith('3)'):
                         data['address'] = line.split(':')[1].strip()
-                        print(data['address'])
                         line = f.readline().strip()
                         if line.startswith('4)'):
-                            data['running_exp'] = line.split(':')[1]
-                            print(data['running_exp'])
+                            data['running_time'] = line.split(':')[1]
                             line = f.readline().strip()
                             if line.startswith('5)'):
-                                data['running_time'] = line.split(':')[1].strip()
-                                print(data['running_time'])
+                                data['running_pace'] = line.split(':')[1].strip()
                                 line = f.readline().strip()
                                 if line.startswith('6)'):
                                     data['entry_path'] = line.split(':')[1].strip()
-                                    print(data['entry_path'])
                                     line = f.readline().strip()
-                                    if line.startswith('20'):
-                                        add_message_data_to_sheet_continuously(data)
+                                    if line.startswith('7)'):
+                                        data['self_ex'] = line.split(':')[1].strip()
+                                        line = f.readline().strip()
+                                    if line.startswith('20') or  line.startswith(''):
+                                        print(data)
+                                        add_message_data_to_sheet(data)
                                         data = {}
-                                        time.sleep(3)
+                                        time.sleep(0.5)
                                         continue      
